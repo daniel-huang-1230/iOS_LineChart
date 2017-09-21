1a035ba7252d1682301430003de39b3150b0a1de
Daniel Huang <gn03249822@danieltekimacbook-pro.local>
2017-09-21 03:26:16 -0700

    UI completed

--- LineChart/ViewController.swift
+++ LineChart/ViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
-
+import Charts
 class ViewController: UIViewController {
 
+    @IBOutlet weak var chartView: LineChartView!
+    
+    var numbers: [Double] = []
+    
+    @IBOutlet weak var textBox: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +27,38 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func goBtnPressed(_ sender: Any) {
+        
+        let input = Double(textBox.text!)   //the input num is expected to be double
+            
+        
+        numbers.append(input!) //here we add the data entered by the user to the array
+        updateGraph()
+    }
+    
+    func updateGraph(){
+        
+        var lineChartEntry = [ChartDataEntry]()
+        
+        //iterate through a
+        for i in 0..<numbers.count{
+            
+            let value = ChartDataEntry(x: Double(i), y: numbers[i])
+            
+            lineChartEntry.append(value)
+            
+        }
+        
+        //now we need to create the actual line
+        
+        let line = LineChartDataSet(values: lineChartEntry, label: "Number")
+        
+        //set the color to yellow
+        
+        line.colors = [NSUIColor.yellow]
+        
+        
+    }
+    
 }
 
