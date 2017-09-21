5c50c0a0927f9e75cee1bcc6c737e5895637a8f1
Daniel Huang <gn03249822@danieltekimacbook-pro.local>
2017-09-21 02:52:06 -0700

    Initial Commit

---
+++ LineChart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LineChart
+//
+//  Created by Daniel Huang on 9/21/17.
+//  Copyright © 2017 Daniel Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
